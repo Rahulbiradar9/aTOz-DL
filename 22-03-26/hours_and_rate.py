6d7b8d5de8ebe642a66e99525d395618d1922ed4
Rahulbiradar9 <rahulbiradar7134@gmail.com>
2026-03-22 19:01:29 +0530

    Python understanding

---
+++ 22-03-26/hours_and_rate.py
@@ -0,0 +1,8 @@
+#hours = float(input("Enter Hours: "))
+#rate = float(input("Enter Rate: "))
+#print("Pay:", hours * rate)
+
+hrs = input("enter hrs:")
+i = float (hrs)
+rate = 2.75
+print ("Pay:",i*rate)
